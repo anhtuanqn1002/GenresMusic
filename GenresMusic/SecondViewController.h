776842a24a42257cf2fbd900b71c65df52a06c9d
Nguyen Van Anh Tuan <anhtuanqn1002@gmail.com>
2015-11-27 14:34:53 +0700

    Initial Commit

---
+++ GenresMusic/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  GenresMusic
+//
+//  Created by Nguyen Van Anh Tuan on 11/27/15.
+//  Copyright © 2015 Nguyen Van Anh Tuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
